e79d705cb900db2bccf7d47d34041d62fa3cf5e3
Joe Sustaric <jsustari@thoughtworks.com>
2014-03-26 09:33:51 +1100

    getting minitest set up

--- Gemfile
+++ Gemfile
@@ -1,1 +1,8 @@
-Gemfile
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rake'
+gem 'guard'
+gem 'minitest'
+gem 'guard-minitest'
+gem 'turn'
--- test/game_test.rb
+++ test/game_test.rb
@@ -1,1 +1,13 @@
-game_test.rb
+require 'minitest/spec'
+require 'minitest/autorun'
+require_relative 'minitest_helper'
+
+describe Array do
+  it "can be created with no arguments" do
+    Array.new.must_be_instance_of Array
+  end
+
+  it "can be created with a specific size" do
+    Array.new(10).size.must_equal 10
+  end
+end
