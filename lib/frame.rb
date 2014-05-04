ce0b6c8930ae92d6290db76a9eca2a1768950ee4
Joe Sustaric <jsustari@thoughtworks.com>
2014-05-04 19:15:10 +1000

    refactoring frame class

--- lib/frame.rb
+++ lib/frame.rb
@@ -10,7 +10,6 @@ class Frame
     if @mode == :complete
       return @rolls.reduce(:+)
     end
-    nil
   end
 
   def register_roll roll
@@ -21,36 +20,51 @@ class Frame
   end
 
   def roll_is_valid? roll
-    if roll < 0 || roll > 10
+    if pins_hit_are_more_than_available_or_negative? roll
       return false
-    elsif @rolls.size==1 && @mode == :normal && roll > (10 - rolls[0])
+    elsif (frame_needs_a_second_roll?) && (pins_hit_are_more_than_remaining_pins? roll)
       return false
     end
     true
   end
 
   def determine_mode
-    if @rolls.size==1 && strike?
+    if strike? || spare?
       @mode = :bonus
-    elsif @rolls.size==2 && spare?
-      @mode = :bonus
-    elsif @rolls.size==2 && not_spare_or_strike?
-      @mode = :complete
-    elsif rolls.size==3
+    elsif two_rolls_hit_less_than_max_pins? || registered_max_rolls?
       @mode = :complete
     end
   end
 
   def strike?
-    @rolls[0]==10
+    (@rolls.size==1 && @rolls[0]==10)
   end
 
   def spare?
-    @rolls[0] + @rolls[1] == 10
+    @rolls.size==2 && (@rolls[0] + @rolls[1] == 10)
   end
 
-  def not_spare_or_strike?
-    @rolls[0] + @rolls[1] < 10
+  def two_rolls_hit_less_than_max_pins?
+    @rolls.size==2 && (@rolls[0] + @rolls[1] < 10)
   end
+
+  def frame_needs_a_second_roll?
+    @rolls.size==1 && @mode == :normal
+  end
+
+  def registered_max_rolls?
+    rolls.size==3
+  end
+
+  def pins_hit_are_more_than_available_or_negative? roll
+    (roll < 0 || roll > 10)
+  end
+
+  def pins_hit_are_more_than_remaining_pins? roll
+    (roll > 0 && roll > (10 - rolls[0]))
+  end
+
+  private :pins_hit_are_more_than_available_or_negative?, :frame_needs_a_second_roll?,
+          :pins_hit_are_more_than_remaining_pins?
 
 end
